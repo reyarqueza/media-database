bdf4dc316b28f4ba68a7d1186dce207bafdb1273
Rey Arqueza <reyarqueza@gmail.com>
2026-02-11 22:48:26 -0800

    ddl

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,5 @@
+-- DDL
+
 BEGIN;
 
 CREATE TABLE users (
@@ -14,11 +16,19 @@ CREATE TABLE media_types (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE media_items (
+CREATE TABLE medias (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     media_type_id INTEGER NOT NULL REFERENCES media_types(id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT medias_unique_title_type UNIQUE (title, media_type_id)
+);
+
+CREATE TABLE users_medias (
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    media_id INTEGER NOT NULL REFERENCES medias(id) ON DELETE CASCADE,
+    added_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, media_id)
 );
 
 END;
