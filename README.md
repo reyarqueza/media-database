93a4e910dc22790b73ab9bd9f530718eaddf7754
Rey Arqueza <reyarqueza@gmail.com>
2026-02-18 11:14:16 -0800

    added readme and updated dml

---
+++ README.md
@@ -0,0 +1,11 @@
+# media-database
+
+This small database was created for educational purposes:
+
+<img src="svg/media_relationships.svg" width="70%" />
+
+## file prefixes
+SQL files are prefixed by **ddl**, **dml**, and **dql** for convenience.
+
+## data examples
+Some medias are ficticious, as well as default users.
--- dml-medias.sql
+++ dml-medias.sql
@@ -19,5 +19,23 @@ INSERT INTO medias (title, media_type_id) VALUES ('Megazone 23 Part 3',
 INSERT INTO medias (title, media_type_id) VALUES ('Megazone 23 Collection',
   (SELECT id FROM media_types WHERE name = 'Blu-ray')
 );
+INSERT INTO medias (title, media_type_id) VALUES ('Attack of the Bionoids',
+  (SELECT id FROM media_types WHERE name = 'VHS')
+);
+INSERT INTO medias (title, media_type_id) VALUES ('Superdimension Fortress Macross',
+  (SELECT id FROM media_types WHERE name = 'LaserDisc')
+);
+INSERT INTO medias (title, media_type_id) VALUES ('Robotech: The Macross Saga',
+  (SELECT id FROM media_types WHERE name = 'DVD')
+);
+INSERT INTO medias (title, media_type_id) VALUES ('Macross Plus - Box Set',
+  (SELECT id FROM media_types WHERE name = 'DVD')
+);
+INSERT INTO medias (title, media_type_id) VALUES ('Macross Zero',
+  (SELECT id FROM media_types WHERE name = 'DVD')
+);
+INSERT INTO medias (title, media_type_id) VALUES ('Macross 7 Collection',
+  (SELECT id FROM media_types WHERE name = 'Blu-ray')
+);
 
 END;
